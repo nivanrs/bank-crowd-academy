5305d11f1db87ece424557aabe1619bd1dd60bcd
Nivan Ramadhan S <nivanrs@gmail.com>
2020-11-19 11:26:11 +0700

    Menambahkan kursus pilhan

--- src/component/KursusPilihan/KursuPilihan.jsx
+++ src/component/KursusPilihan/KursuPilihan.jsx
@@ -7,42 +7,42 @@ export default function KursusPilihan(props) {
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title>Investasi untuk Pemula</Card.Title>
           <Card.Text>
             This is a wider card with supporting text below as a natural lead-in
             to additional content. This content is a little bit longer.
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title>Perencanaan Keuangan untuk Pemula</Card.Title>
           <Card.Text>
             This card has supporting text below as a natural lead-in to
             additional content.{" "}
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title>Paham Laporan Keuangan</Card.Title>
           <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
+            This card has supporting text below as a natural lead-in to
+            additional content.{" "}
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
+      </Card>      
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Belajar Akuntansi dengan Mudah</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{" "}
+          </Card.Text>
+        </Card.Body>
       </Card>
     </CardDeck>
   );
