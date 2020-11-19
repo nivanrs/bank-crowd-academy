0d3a5e12662ade783b5a76fd22f6e120b071d640
rezaahmadk <rezaahmadk@gmail.com>
2020-11-19 11:46:59 +0700

    Change Login Process

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,27 +1,55 @@
+import React from 'react';
+import axios from 'axios';
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 
-export default function Login(props) {
-  return (
-    <Container>
-    <h2>MASUK</h2>
-    <hr />
-    <br/>
+export default class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
 
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Masukkan Email" />
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Massukan Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Masuk
-      </Button>
-    </Form>
-</Container>
-  );
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios.post(`http://localhost:9000/api/login`, user, {headers: {"Content-Type" : "application/json"}})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <Container>
+        <h2>MASUK</h2>
+        <hr />
+        <br/>
+
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Masukkan Email" name="email" onChange={this.handleChange} />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Masukkan Password" name="password" onChange={this.handleChange} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Masuk
+          </Button>
+        </Form>
+      </Container>
+    )
+  }
 }
